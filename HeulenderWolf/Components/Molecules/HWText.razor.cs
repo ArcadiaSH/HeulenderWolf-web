7857542893c1370995709a6022ff2ebff786881b
nigerade <nicholasgerade@gmail.com>
2025-10-21 21:32:37 -0300

    Add first section

---
+++ HeulenderWolf/Components/Molecules/HWText.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace HeulenderWolf.Components.Molecules
+{
+    public partial class HWButton : ComponentBase
+    {
+        #region PARAMETROS
+        [Parameter] public string Texto { get; set; } = "Compre agora";
+        [Parameter] public string Grid { get; set; } = "6";
+        [Parameter] public EventCallback Evento { get; set; }
+        #endregion
+
+        #region METODOS
+        protected void OnClick()
+        {
+            Evento.InvokeAsync();
+        }
+        #endregion
+    }
+}
