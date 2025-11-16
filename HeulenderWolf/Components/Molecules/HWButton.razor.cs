f8a1d05deb756f6d437c3e8328ed5c936163caa5
Guilherme Sanches Romano <gui.romano.dev@gmail.com>
2025-11-16 18:03:10 -0300

    compoentes

--- HeulenderWolf/Components/Molecules/HWButton.razor.cs
+++ HeulenderWolf/Components/Molecules/HWButton.razor.cs
@@ -8,7 +8,12 @@ namespace HeulenderWolf.Components.Molecules
         [Parameter] public string Texto { get; set; } = "Compre agora";
         [Parameter] public string Grid { get; set; } = "6";
         [Parameter] public EventCallback Evento { get; set; }
-        [Parameter] public string? CSSButton { get; set; } 
+        [Parameter] public string? CSSButton { get; set; }
+        [Parameter] public bool Outline { get; set; } = false;
+        #endregion
+
+        #region PROPRIEDADE
+
         #endregion
 
         #region METODOS
--- HeulenderWolf/Components/Molecules/HWCard.razor.cs
+++ HeulenderWolf/Components/Molecules/HWCard.razor.cs
@@ -5,7 +5,9 @@ namespace HeulenderWolf.Components.Molecules
     public partial class HWCard : ComponentBase
     {
         #region PARAMETRO
-
+        [Parameter] public RenderFragment? CardBody { get; set; }
+        [Parameter] public bool Pequeno { get; set; }
+        [Parameter] public string? CSSColor { get; set;}
         #endregion
 
     }
