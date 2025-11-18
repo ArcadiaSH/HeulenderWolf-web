85deb0fbe62e3b2fccab3ba5c8720bbd085ee03a
Guilherme Sanches Romano <gui.romano.dev@gmail.com>
2025-11-17 21:36:16 -0300

    botao

---
+++ HeulenderWolf/Models/HWButtonModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace HeulenderWolf.Models
+{
+    public class HWButtonModel
+    {
+         public string Texto { get; set; } = "Compre agora";
+         public string ButttonFontColor { get; set; } = "#fff";
+         public string ButtonFontWeight { get; set; } = "700";
+         public string? ButtonColor { get; set; } = "#fcb130";
+         public bool Outline { get; set; } = false;
+
+    }
+}
